d427b49b252ee6058e07e1e50b02972d57701590
Bemar1 <Bemar1@BEMAR>
2024-03-16 15:31:04 +0300

    arrays

---
+++ Arrays/Program.cs
@@ -0,0 +1,32 @@
+﻿string[] yapiKredi = { "Taşıt Kredisi", "İhtiyaç Kredisi", "Konut Kredisi", "Kobi" };
+string[] garanti = { "Taşıt Kredisi"};
+string[] akbank = { "Taşıt Kredisi", "İhtiyaç Kredisi"};
+
+
+Console.WriteLine("Kredi çekmek istediğiniz banka: ");
+string bankName = Console.ReadLine();
+
+if (bankName.Contains("yapi"))
+{
+    foreach (var item in yapiKredi)
+    {
+        Console.WriteLine(item);
+    }
+}else if (bankName.Contains("ak"))
+{
+    foreach (var item in akbank)
+    {
+        Console.WriteLine(item);
+    }
+}
+else
+{
+    foreach (var item in garanti)
+    {
+        Console.WriteLine(item);
+    }
+}
+
+
+
+
